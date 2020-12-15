fa4f03cd5aaec5cd774b14b36dce9bd5ec1fed61
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-12-15 14:22:10 -0600

    templating

--- mernMeadUdemy/web-server/src/app.js
+++ mernMeadUdemy/web-server/src/app.js
@@ -4,8 +4,30 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs')
+
 const publicDirPath = path.join(__dirname, '../public/');
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Coby Burckard'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Coby Burckard'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'get help here'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
