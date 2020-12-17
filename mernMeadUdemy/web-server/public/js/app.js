54cdac6482e0842e4ea0424bc57c7ebeb57e15ad
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-12-17 10:03:46 -0600

    finishing weather app

--- mernMeadUdemy/web-server/public/js/app.js
+++ mernMeadUdemy/web-server/public/js/app.js
@@ -1,1 +1,19 @@
 console.log('client side js loaded')
+
+const fetchWeather = async (location) => {
+  const response = await fetch(`/weather?address=${ location }`)
+  const data = await response.json()
+  if (data.error) {
+    console.log(data.error)
+  }
+  console.log(data)
+}
+
+window.onload = () => {
+  const weatherForm = document.querySelector('form')
+  const search = document.querySelector('input')
+  weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetchWeather(search.value)
+  })
+}
